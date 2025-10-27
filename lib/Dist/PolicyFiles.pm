75d5e3121996f35956e3659c431832787420423e
Klaus Rindfrey <k-rindfrey@gmx.de>
2025-10-27 11:48:25 +0100

    adapted version

--- lib/Dist/PolicyFiles.pm
+++ lib/Dist/PolicyFiles.pm
@@ -6,7 +6,7 @@ use warnings;
 
 use feature ':5.10';
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 
 use Carp;
@@ -160,7 +160,7 @@ Dist::PolicyFiles - Generate CONTRIBUTING.md and SECURITY.md
 
 =head1 VERSION
 
-Version 0.02
+Version 0.03
 
 =head1 SYNOPSIS
 
